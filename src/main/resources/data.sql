d03ff0432e6fde272486861c6bd43301ad626bd0
Peter Drab <drabpe@sheridancollege.ca>
2021-06-18 21:58:19 -0400

    Fixed error lacking imports / added sql schema + initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO pet
+(pet_name, pet_kind, pet_gender, vaccinated)
+VALUES
+('Elmo','dog','female',false),
+('Mickey','rabbit','male',true),
+('Jake','cat','female',false),
+('Rowen','dog','male',true);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pet (
+                     id INT AUTO_INCREMENT PRIMARY KEY,
+                     pet_name VARCHAR(30),
+                     pet_kind VARCHAR(30),
+                     pet_gender VARCHAR(30),
+                     vaccinated BOOLEAN
+);
